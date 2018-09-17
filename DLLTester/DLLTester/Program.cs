ca92565a9c3a1025ab012f94161769fbf578c2fe
DanielEASJ <31865128+DanielEASJ@users.noreply.github.com>
2018-09-17 12:42:48 +0200

    commit base

---
+++ DLLTester/DLLTester/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewMyMathDll;
+
+namespace DLLTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathy m = new Mathy();
+            Console.WriteLine(m.Add(2,4));
+            Console.WriteLine(m.Add2(10,100,1000));
+
+
+            Console.ReadLine();
+        }
+    }
+}
